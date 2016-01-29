332941277a1fda66fb204c0ffeab89ef8c85b41b
Eric White <ericwhite613@gmail.com>
2016-01-29 15:18:02 -0500

    Whwre am i

--- .travis/push.sh
+++ .travis/push.sh
@@ -9,7 +9,9 @@ setup_git() {
 publish_gh_pages() {
   git push origin --delete <branchName>
   git checkout --orphan gh-pages && rm -rf `ls -a | grep -vE '\.gitignore|\.git|node_modules|bower_components|(^[.]{1,2}/?$)'` && git add -A && git commit -m "initial gh-pages commit"
+  push origin -u gh-pages
   git branch -r
+  basename $(git remote show -n origin | grep Fetch | cut -d: -f2-)
   git checkout dev
   ember github-pages:commit --message "[ci skip] Update gh-pages"
   git push
