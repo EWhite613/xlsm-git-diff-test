65bdfc4c0c405140d3eb97a24bf328a4c5e3c669
Eric White <ericwhite613@gmail.com>
2016-01-29 13:54:00 -0500

    Tokens

--- .travis/push.sh
+++ .travis/push.sh
@@ -7,7 +7,9 @@ setup_git() {
 
 
 publish_gh_pages() {
-  git fetch origin
+  git fetch
+  git checkout gh-pages 
+  git branch -r
   git checkout dev
   ember github-pages:commit --message "[ci skip] Update gh-pages"
   git push --set-upstream origin gh-pages
