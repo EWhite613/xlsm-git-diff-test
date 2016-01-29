7d9660dc52acff827f030e2b64a98fd85fc38ed6
Eric White <ericwhite613@gmail.com>
2016-01-29 16:00:40 -0500

    Revert "Make less destructive"
    This reverts commit fd6b2a0966d1c20b39e96daec914d5f094c3ab37.

--- .travis/push.sh
+++ .travis/push.sh
@@ -7,8 +7,15 @@ setup_git() {
 
 
 publish_gh_pages() {
-  git fetch origin
+  git push origin --delete gh-pages
+  git checkout --orphan gh-pages 
+  rm -rf `ls -a | grep -vE '\.gitignore|\.git|node_modules|bower_components|(^[.]{1,2}/?$)'`
+  git add -A
+  git commit -m "initial gh-pages commit"
+  git push origin -u gh-pages
   git branch -r
+  basename $(git remote show -n origin | grep Fetch | cut -d: -f2-)
+  git checkout dev
   ember install ember-cli-github-pages
   git commit -am "[ci skip] install github-pages"
   ember github-pages:commit --message "[ci skip] Update gh-pages"
