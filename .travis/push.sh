b6476987caaaa41a0b73ee73c2ceb6ac9d2580d8
Eric White <ericwhite613@gmail.com>
2016-01-29 15:42:07 -0500

    Separate lines

--- .travis/push.sh
+++ .travis/push.sh
@@ -8,10 +8,11 @@ setup_git() {
 
 publish_gh_pages() {
   git push origin --delete gh-pages
-  git checkout --orphan gh-pages && rm -rf `ls -a | grep -vE '\.gitignore|\.git|node_modules|bower_components|(^[.]{1,2}/?$)'` && git add -A && git commit -m "initial gh-pages commit"
-  git push origin -u gh-pages
+  git checkout --orphan gh-pages 
+  rm -rf `ls -a | grep -vE '\.gitignore|\.git|node_modules|bower_components|(^[.]{1,2}/?$)'`
+  git add -A
+  git commit -m "initial gh-pages commit"
   git branch -r
-  basename $(git remote show -n origin | grep Fetch | cut -d: -f2-)
   git checkout dev
   ember install ember-cli-github-pages
   git commit -am "[ci skip] install github-pages"
