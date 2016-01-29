34c27e62c4b1926033b0206886634858999fe6f9
Eric White <ericwhite613@gmail.com>
2016-01-29 15:06:09 -0500

    Super overkill

--- .travis/push.sh
+++ .travis/push.sh
@@ -7,15 +7,12 @@ setup_git() {
 
 
 publish_gh_pages() {
-  git checkout -b gh-pages -set-upstream origin gh-pages
-  git pull
-  rm -rf `ls -a | grep -vE '\.gitignore|\.git|node_modules|bower_components|(^[.]{1,2}/?$)'` && git add -A && git commit -m "initial gh-pages commit"
+  git push origin --delete <branchName>
+  git checkout --orphan gh-pages && rm -rf `ls -a | grep -vE '\.gitignore|\.git|node_modules|bower_components|(^[.]{1,2}/?$)'` && git add -A && git commit -m "initial gh-pages commit"
   git branch -r
   git checkout dev
-  git fetch
   ember github-pages:commit --message "[ci skip] Update gh-pages"
-  git push --set-upstream origin gh-pages
-}
+  git push
 
 setup_git
 publish_gh_pages
