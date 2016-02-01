35ba9319c4e76c9c5823700aa422c4efe24e4c8c
Eric White <ericwhite613@gmail.com>
2016-02-01 08:43:39 -0500

    Test proper clone

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,13 +3,12 @@
 setup_git() {
   git config --global user.email "ericwhite613@gmail.com"
   git config --global user.name "Eric White"
-  git clone --branch=$branch $repo
-  git checkout $sha1
+  git clone --branch=dev https://github.com/EWhite613/xlsm-git-diff-test.git
+  git checkout dev
 }
 
 
 publish_gh_pages() {
-  git checkout dev
   ember install ember-cli-github-pages
   git commit -am "[ci skip] install github-pages"
   ember github-pages:commit --message "[ci skip] Update gh-pages"
