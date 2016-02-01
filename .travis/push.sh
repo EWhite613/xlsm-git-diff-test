1aa0e4ecb30666185a4941871c2763250976153f
Eric White <ericwhite613@gmail.com>
2016-02-01 08:34:23 -0500

    Test less destruction

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,18 +3,12 @@
 setup_git() {
   git config --global user.email "ericwhite613@gmail.com"
   git config --global user.name "Eric White"
+  git clone --branch=$branch $repo
+  git checkout $sha1
 }
 
 
 publish_gh_pages() {
-  git push origin --delete gh-pages
-  git checkout --orphan gh-pages 
-  rm -rf `ls -a | grep -vE '\.gitignore|\.git|node_modules|bower_components|(^[.]{1,2}/?$)'`
-  git add -A
-  git commit -m "initial gh-pages commit"
-  git push origin -u gh-pages
-  git branch -r
-  basename $(git remote show -n origin | grep Fetch | cut -d: -f2-)
   git checkout dev
   ember install ember-cli-github-pages
   git commit -am "[ci skip] install github-pages"
