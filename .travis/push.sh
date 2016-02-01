27260fda3a3c738897584056124979b7b941762c
Eric White <ericwhite613@gmail.com>
2016-02-01 08:52:38 -0500

    Bring in gh-pages

--- .travis/push.sh
+++ .travis/push.sh
@@ -4,7 +4,10 @@ setup_git() {
   git config --global user.email "ericwhite613@gmail.com"
   git config --global user.name "Eric White"
   git clone --branch=dev https://github.com/EWhite613/xlsm-git-diff-test.git
+  git fetch origin
+  git checkout -b gh-pages origin/gh-pages
   git checkout dev
+  
 }
 
 
